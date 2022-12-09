d7c95c25cb461864db137e28db486ad1fbd6ad10
jojongguk <jojongguk@factorial.games>
2022-12-09 20:14:51 +0900

    rafactoring day 03

--- src/day_03.rs
+++ src/day_03.rs
@@ -1,15 +1,8 @@
 pub fn solve_part1(input: &str) -> u32 {
-    let bits = count_bits(input);
-
-    let mut gamma_rate = 0;
-    let mut epsilon_rate = 0;
-    for c in &bits {
-        gamma_rate <<= 1;
-        gamma_rate |= if *c > 0 { 1 } else { 0 };
-
-        epsilon_rate <<= 1;
-        epsilon_rate |= if *c < 0 { 1 } else { 0 };
-    }
+    let count = bit_count(input);
+    let input_bits = input_to_bits(input);
+    let gamma_rate = most_common_bits(count, &input_bits);
+    let epsilon_rate = least_common_bits(count, &input_bits);
 
     gamma_rate * epsilon_rate
 }
@@ -18,34 +11,25 @@ pub fn solve_part2(input: &str) -> u32 {
     let count = bit_count(input);
 
     // oxygen
-    let mut bits = input_to_bits(input);
+    let mut remained = input_to_bits(input);
     let mut oxygen = 0;
     for bit_idx in (0..count).rev() {
-        let common = most_common(count, &bits);
-        bits = bits
-            .iter()
-            .filter(|&bit| (bit & 1 << bit_idx) == (common & 1 << bit_idx))
-            .copied()
-            .collect();
-        if bits.len() == 1 {
-            oxygen = bits[0];
+        let most_bits = most_common_bits(count, &remained);
+        remained.retain(|&bit| (bit & 1 << bit_idx) == (most_bits & 1 << bit_idx));
+        if remained.len() == 1 {
+            oxygen = remained[0];
             break;
         }
     }
 
     // co2
-    let mut bits = input_to_bits(input);
+    let mut remained = input_to_bits(input);
     let mut co2 = 0;
     for idx in (0..count).rev() {
-        let least = least_common(count, &bits);
-        bits = bits
-            .iter()
-            .filter(|&bit| bit & 1 << idx == least & 1 << idx)
-            .copied()
-            .collect();
-
-        if bits.len() == 1 {
-            co2 = bits[0];
+        let least_bits = least_common_bits(count, &remained);
+        remained.retain(|&bit| bit & 1 << idx == least_bits & 1 << idx);
+        if remained.len() == 1 {
+            co2 = remained[0];
             break;
         }
     }
@@ -69,31 +53,12 @@ fn to_bits(input: &str) -> u32 {
     let mut result = 0;
     for c in input.chars() {
         result <<= 1;
-        result |= if c == '1' { 1 } else { 0 };
+        result |= u32::from(c == '1');
     }
     result
 }
 
-fn count_bits(input: &str) -> Vec<i32> {
-    let first = input.lines().next().unwrap().trim();
-    let count = first.len();
-    let mut common = Vec::new();
-    common.resize(count, 0);
-
-    for line in input.lines() {
-        let line = line.trim();
-        for (idx, c) in line.char_indices() {
-            if c == '0' {
-                common[idx] -= 1;
-            } else {
-                common[idx] += 1;
-            }
-        }
-    }
-    common
-}
-
-fn most_common(len: u32, bits: &Vec<u32>) -> u32 {
+fn most_common_bits(len: u32, bits: &Vec<u32>) -> u32 {
     let mut result = 0;
     for bit_idx in 0..len {
         let mut count_1 = 0;
@@ -113,7 +78,7 @@ fn most_common(len: u32, bits: &Vec<u32>) -> u32 {
     result
 }
 
-fn least_common(len: u32, bits: &Vec<u32>) -> u32 {
+fn least_common_bits(len: u32, bits: &Vec<u32>) -> u32 {
     let mut result = 0;
     for bit_idx in 0..len {
         let mut count_1 = 0;
