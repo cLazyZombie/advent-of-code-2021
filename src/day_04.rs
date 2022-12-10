994abbbc6903662e782e6ef1ba95f3fadfe80489
jojongguk <jojongguk@factorial.games>
2022-12-10 11:14:02 +0900

    day 04 part2

--- src/day_04.rs
+++ src/day_04.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 pub fn solve_part1(input: &str) -> u32 {
     let (numbers, mut grids) = parse_input(input);
 
@@ -7,6 +9,32 @@ pub fn solve_part1(input: &str) -> u32 {
             if is_winner(grid) {
                 let sum: u32 = sum_unmarked(grid);
                 return sum * n;
+            }
+        }
+    }
+
+    0
+}
+
+pub fn solve_part2(input: &str) -> u32 {
+    let (numbers, mut grids) = parse_input(input);
+
+    let grids_count = grids.len();
+
+    let mut winned = HashSet::new();
+    for n in numbers {
+        for (idx, grid) in grids.iter_mut().enumerate() {
+            if winned.contains(&idx) {
+                continue;
+            }
+
+            mark(n, grid);
+            if is_winner(grid) {
+                winned.insert(idx);
+                if winned.len() == grids_count {
+                    let sum: u32 = sum_unmarked(grid);
+                    return sum * n;
+                }
             }
         }
     }
@@ -136,6 +164,38 @@ mod tests {
     fn test_part1() {
         let input = include_str!("../input/day_04.txt");
         let answer = solve_part1(input);
+        assert_eq!(answer, 39984);
+    }
+
+    #[test]
+    fn test_part2_sample() {
+        let input = r#"7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+        22 13 17 11  0
+         8  2 23  4 24
+        21  9 14 16  7
+         6 10  3 18  5
+         1 12 20 15 19
+        
+         3 15  0  2 22
+         9 18 13 17  5
+        19  8  7 25 23
+        20 11 10 24  4
+        14 21 16 12  6
+        
+        14 21 17 24  4
+        10 16 15  9 19
+        18  8 23 26 20
+        22 11 13  6  5
+         2  0 12  3  7"#;
+        let answer = solve_part2(input);
+        assert_eq!(answer, 1924);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = include_str!("../input/day_04.txt");
+        let answer = solve_part2(input);
         assert_eq!(answer, 39984);
     }
 }
